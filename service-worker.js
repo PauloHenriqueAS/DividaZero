6dc482f8e799a6f86a70e7221293a59dbca0604f
Matheus Reis <matheusjosereis@hotmail.com>
2023-09-14 14:02:22 -0300

    Fazendo validação dos campos de cadastro do usuário

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,39 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+const CACHE_NAME = 'my-pwa-cache';
+const cacheUrls = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+];
 
-workbox.routing.registerRoute(
-    ({request}) => request.destination == 'image',
-    new workbox.strategies.NetworkFirst()
-);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(cacheUrls);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
--- src/userRegistration.js
+++ src/userRegistration.js
@@ -7,17 +7,86 @@ function searchAdress(){
         setAddressAutomatically(cep);
 }
 
-function setAddressAutomatically(cep){
+
+function getUserRegistrationFields(){
+    return {
+        'fullName' : $('#userFullname').val().trim(),
+        'email' : $('#userEmail').val().trim(),
+        'citizenship' : $('#userCitizenship').val().trim(),
+        'address': {
+            'cep' : $('#userCep').val().trim(),
+            'streetName' : $('#userStreetName').val().trim(),
+            'neighborhood' : $('#userNeighborhood').val().trim(),
+            'complement' : $('#userComplement').val().trim(),
+        },
+        'houseNumber' : $('#userHouseNumber').val(),
+        'password' : $('#userPassword').val().trim(),
+        'passwordConfirmation' : $('#userPasswordConfirmation').val().trim(),        
+    }
+}
+
+
+function verifyUserRegistrationFields() {
+    return new Promise((resolve, reject) => {
+      const user = getUserRegistrationFields();
+      const areFieldsIncomplete =
+        user.fullName == '' ||
+        user.email == '' ||
+        user.citizenship == '' ||
+        user.address.cep == '' ||
+        user.address.streetName == '' ||
+        user.address.neighborhood == '' ||
+        user.address.complement == '' ||
+        user.houseNumber == '' ||
+        user.password == '' ||
+        user.passwordConfirmation == '';
+  
+      if (user.password != user.passwordConfirmation) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Senhas não coincidem!',
+        }).then(() => {
+          reject('Senhas não coincidem!');
+        });
+      }else if (areFieldsIncomplete) {
+        Swal.fire({
+          icon: 'warning',
+          title: 'Oops...',
+          text: 'Preencha todos os campos!',
+        }).then(() => {
+          reject('Campos incompletos!');
+        });
+      } else {
+        resolve(true);
+      }      
+    });
+}
+
+function setAddressAutomatically(cep){    
     const viaCepUrl = `https://viacep.com.br/ws/${cep}/json/`
 
     fetch(viaCepUrl)
         .then((response) => response.json())
         .then((result) => {
-            $('#streetName').val(result.logradouro);
-            $('#complement').val(result.complemento);
-            $('#neighborhood').val(result.bairro);
+            $('#userStreetName').val(result.logradouro);
+            $('#userComplement').val(result.complemento);
+            $('#userNeighborhood').val(result.bairro);
         })
         .catch((error) => {
             reject(error);
+        });
+}
+
+function finishUserRegistration() {
+    verifyUserRegistrationFields()
+        .then((result) => {
+            console.log(result)
+            if (result) {
+            console.log('Chamar api para cadastro');
+            }
+        })
+        .catch((error) => {
+            console.error(`Erros de validação:${error}`);
         });
 }
