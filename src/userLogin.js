80e226d90f53e2c29c2cffb9f3186c2e7f313b13
flaviozno <flavio.vezono@ufu.br>
2023-09-15 03:31:00 -0300

    fix promise and add fetch

---
+++ src/userLogin.js
@@ -0,0 +1,42 @@
+import { URL_API_BASE } from "./Extensions/constants.js";
+
+const getUserLoginFields = () => {
+  return {
+    email: $("#userEmailLogin").val().trim(),
+    password: $("#userPasswordLogin").val().trim(),
+  };
+};
+
+const verifyUserLoginFields = () => {
+  return new Promise((resolve, reject) => {
+    const user = getUserLoginFields();
+    if (user && user.password && user.password.length > 0) {
+      resolve(user);
+    }
+    reject("Sua senha está incorreta!");
+  });
+};
+
+const handleSubmit = () => {
+  verifyUserLoginFields()
+    .then(async (result) => {
+      if (result) {
+        console.log("Chamar API para login de usuario");
+        try {
+          const res = await fetch(`${URL_API_BASE}/user/login`, {
+            method: "POST",
+            body: result,
+          });
+
+          const resData = await res.json();
+
+          console.log(resData);
+        } catch (err) {
+          console.log(err.message);
+        }
+      }
+    })
+    .catch((error) => {
+      console.log(`Erros de validação:${error}`);
+    });
+};
