244e173d94f6c7006fdecb7f0c36ce47cd6b0bc9
flaviozno <flavio.vezono@ufu.br>
2023-09-17 12:45:26 -0300

    download

--- src/renegotiation.js
+++ src/renegotiation.js
@@ -1,7 +1,59 @@
-function redirectToRenegotiation(){
-    window.location.href = 'form_term.html';
-}
+// function redirectToRenegotiation(){
+//     window.location.href = 'form_term.html';
+// }
 
-function generateRenegotiation(){
-    window.location.href = 'pagamento.html';
+// function generateRenegotiation(){
+//     window.location.href = 'pagamento.html';
+// }
+
+// const getDados = ()=> {
+
+// }
+
+// function redirectToRenegotiation(){
+//     window.location.href = 'form_term.html';
+// }
+
+async function generateRenegotiation(){
+    const body = {
+        "termo": {
+          "id_termo": 37,
+          "id_divida": 1,
+          "num_parcela": 1,
+          "data_renegociacao": "2023-09-17"
+        },
+        "parcelas": {
+          "valor": 2500.00,
+          "qtd": 1
+        }
+      };
+
+    let requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      };
+      const res = await fetch(`${URL_API_BASE}/term/PostTerm`, requestOptions);
+      const blob = await res.blob();
+
+      const blobUrl = window.URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = blobUrl;
+      link.download = 'Termo.pdf';
+      link.style.display = 'none';
+
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+
+      window.URL.revokeObjectURL(blobUrl);
+      Swal.fire({
+        icon: "success",
+        title: "Sucesso",
+        text: "Termo gerado com sucesso!",
+      }).then(() =>{window.location.reload()})
 }
